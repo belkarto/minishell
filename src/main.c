c326d3343a17a8c24fee8c1511ea8c424260f2bf
Brahim Elkartouch <belkarto@e2r3p12.1337.ma>
2023-02-22 17:38:35 +0100

    add a little test to test makefile

--- src/main.c
+++ src/main.c
@@ -6,8 +6,16 @@
 /*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 14:33:28 by belkarto          #+#    #+#             */
-/*   Updated: 2023/02/22 14:35:41 by belkarto         ###   ########.fr       */
+/*   Updated: 2023/02/22 17:26:27 by belkarto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../include/minishell.h"
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	ft_putstr_fd("test\n", 1);
+	return EXIT_SUCCESS;
+}
