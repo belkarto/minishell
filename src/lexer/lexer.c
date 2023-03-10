b131eef0d7ebd4ffe3bdb673c9ead42864910313
Brahim Elkartouch <belkarto@e2r9p10.1337.ma>
2023-03-10 08:50:32 +0100

    modifier files and its struct

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,120 +6,37 @@
 /*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 15:05:28 by belkarto          #+#    #+#             */
-/*   Updated: 2023/03/08 23:34:50 by belkarto         ###   ########.fr       */
+/*   Updated: 2023/03/10 05:26:55 by belkarto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+#include <readline/history.h>
 
-int	word(t_elem **list, int status, char *line)
+char	*ft_read_line(int status)
 {
-	int	i;
-
-	i = 0;
-	while (line[i])
-	{
-		if (line[i] == SPAC || line[i] == PIPE || line[i] == ENV 
-			|| line[i] == LESS || line[i] == GREAT || line[i] == QUOTE
-			|| line[i] == DQUOTE)
-			break ;
-		i++;
-	}
-	elem_add_tail(list, new_elem(ft_substr(line, 0, i), i, WORD, status));
-	return (i);
-}
-
-int	in_quote(t_elem **list, int *status, int picker)
-{
-	if (picker == 0)
-	{
-		if (*status == GENERAL)
-		{
-			elem_add_tail(list, new_elem(ft_strdup("\'"), 1, QUOTE, *status));
-			*status = IN_QUOTE;
-			return (1);
-		}
-		else if (*status == IN_DQUOTE)
-			*status = IN_DQUOTE;
-		else
-			*status = GENERAL;
-		(void)list;
-		elem_add_tail(list, new_elem(ft_strdup("\'"), 1, QUOTE, *status));
-	}
+	if (status == IN_QUOTE)
+		return (readline("quote>"));
 	else
-	{
-		if (*status == GENERAL)
-		{
-			elem_add_tail(list, new_elem(ft_strdup("\"") , 1, DQUOTE, *status));
-			*status = IN_DQUOTE;
-			return (1);
-		}
-		else if (*status == IN_QUOTE)
-			*status = IN_QUOTE;
-		else
-			*status = GENERAL;
-		elem_add_tail(list, new_elem(ft_strdup("\"") , 1, DQUOTE, *status));
-	}
-	return (1);
+		return (readline("dquote>"));
 }
 
-int	tokens(t_elem **list, int *status, int token)
+void	get_rest(t_elem **list, int statu)
 {
-	if (token == PIPE)
-		elem_add_tail(list, new_elem(ft_strdup("|") , 1, PIPE, *status));
-	else if (token == LESS)
-		elem_add_tail(list, new_elem(ft_strdup("<") , 1, LESS, *status));
-	else if (token == GREAT)
-		elem_add_tail(list, new_elem(ft_strdup(">") , 1, GREAT, *status));
-	return (1);
-}
-
-int	env(t_elem **list, int *status, char *line)
-{
-	int	i;
+	char	*line;
+	int		i;
 
 	i = 0;
-	while (line[++i])
+	line = ft_read_line(statu);
+	if (line == NULL)
 	{
-		if (ft_isalnum(line[i]) || line[i] == '_')
-			continue;
-		else
-			break;
+		ft_putstr_fd("unexpected EOF while looking for matching\n", 2);
+		exit (258);
 	}
-	elem_add_tail(list, new_elem(ft_substr(line, 0, i), i, ENV, *status));
-	return (i);
-}
-
-int	token(t_elem **list, int *status, char *line)
-{
-	if (ft_isalnum(*line) || *line == '_')
-		return (word(list, *status, line));
-	else if (*line == PIPE)
-		return(tokens(list, status, PIPE));
-	else if (*line == LESS)
-		return(tokens(list, status, LESS));
-	else if (*line == GREAT)
-		return(tokens(list, status, GREAT));
-	else if (*line == ENV)
-		return(env(list, status, line));
-	else if (*line == QUOTE)
-	{
-		return (in_quote(list, status, 0));
-	}
-	else if (*line == DQUOTE)
-		return (in_quote(list, status, 1));
-	return (1);
-}
-
-void	printf_list(t_elem *head)
-{
-	ft_printf("|content\t\tlen\t\ttype\t\tstate\n");
-	ft_printf("---------------------------------------------------------\n");
-	while (head)
-	{
-		ft_printf("|%s\t\t%d\t\t%d\t\t%d\n", head->content, head->len, head->type, head->state);
-		head = head->next;
-	}
+	while (line[i])
+		i += token(list, &statu, line + i);
+	if (statu != GENERAL)
+		get_rest(list, statu);
 }
 
 t_elem	*lexer(char *line)
@@ -132,9 +49,8 @@ t_elem	*lexer(char *line)
 	statu = GENERAL;
 	i = 0;
 	while (line[i])
-	{
 		i += token(&head, &statu, line + i);
-	}
-	printf_list(head);
-	return (NULL);
+	if (statu != GENERAL)
+		get_rest(&head, statu);
+	return (head);
 }
---
+++ src/lexer/tokens.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 05:00:17 by belkarto          #+#    #+#             */
+/*   Updated: 2023/03/10 05:02:13 by belkarto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int	word(t_elem **list, int status, char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == SPAC || line[i] == PIPE || line[i] == ENV
+			|| line[i] == LESS || line[i] == GREAT || line[i] == QUOTE
+			|| line[i] == DQUOTE)
+			break ;
+		i++;
+	}
+	elem_add_tail(list, new_elem(ft_substr(line, 0, i), i, WORD, status));
+	return (i);
+}
+
+int	tokens(t_elem **list, int *status, int token)
+{
+	if (token == PIPE)
+		elem_add_tail(list, new_elem(ft_strdup("|"), 1, PIPE, *status));
+	else if (token == LESS)
+		elem_add_tail(list, new_elem(ft_strdup("<"), 1, LESS, *status));
+	else if (token == GREAT)
+		elem_add_tail(list, new_elem(ft_strdup(">"), 1, GREAT, *status));
+	else if (token == SPAC)
+		elem_add_tail(list, new_elem(ft_strdup(" "), 1, SPAC, *status));
+	return (1);
+}
+
+int	env(t_elem **list, int *status, char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[++i])
+	{
+		if (ft_isalnum(line[i]) || line[i] == '_')
+			continue ;
+		else
+			break ;
+	}
+	elem_add_tail(list, new_elem(ft_substr(line, 0, i), i, ENV, *status));
+	return (i);
+}
+
+int	token(t_elem **list, int *status, char *line)
+{
+	if (ft_isalnum(*line) || *line == '_')
+		return (word(list, *status, line));
+	else if (*line == PIPE)
+		return (tokens(list, status, PIPE));
+	else if (*line == LESS)
+		return (tokens(list, status, LESS));
+	else if (*line == GREAT)
+		return (tokens(list, status, GREAT));
+	else if (*line == SPAC)
+		return (tokens(list, status, SPAC));
+	else if (*line == ENV)
+		return (env(list, status, line));
+	else if (*line == QUOTE)
+		return (in_quote(list, status, 0));
+	else if (*line == DQUOTE)
+		return (in_quote(list, status, 1));
+	return (1);
+}
