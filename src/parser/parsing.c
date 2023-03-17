5fa3b2888c3cd985c61ed1ae4155893f1672ed1d
Oumaima Halim <ohalim@e2r2p7.1337.ma>
2023-03-17 22:54:01 +0100

    Update

---
+++ src/parser/parsing.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 18:32:49 by belkarto          #+#    #+#             */
+/*   Updated: 2023/03/13 05:21:02 by belkarto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include <stdlib.h>
+
+void	parsing_input(char *input)
+{
+	t_elem	*head;
+
+	if (input == NULL)
+		end_of_file();
+	head = lexer(input);
+	//print_lexer(head);
+	elem_clear(head);
+}
