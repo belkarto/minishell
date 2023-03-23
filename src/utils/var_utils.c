68c1fa1e1dab2fc800a6b6ba9c9502661268b5a3
brahim elkartouch <belkarto@student.1337.ma>
2023-03-23 01:00:51 +0000

    change all ft_strncmp to ft_strcmp

--- src/utils/var_utils.c
+++ src/utils/var_utils.c
@@ -6,7 +6,7 @@
 /*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 03:19:31 by belkarto          #+#    #+#             */
-/*   Updated: 2023/03/18 08:52:52 by belkarto         ###   ########.fr       */
+/*   Updated: 2023/03/23 00:59:01 by brahim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,11 @@
 t_env	*get_var(char *name, t_env *list)
 {
 	t_env	*tmp;
-	size_t	len;
 
-	len = ft_strlen(name);
 	tmp = list;
 	while (tmp)
 	{
-		if (len < ft_strlen(tmp->content))
-			len = ft_strlen(tmp->content);
-		if (ft_strncmp(tmp->name, name, len) == 0)
+		if (ft_strcmp(tmp->name, name) == 0)
 			return (tmp);
 		tmp = tmp->next;
 	}
