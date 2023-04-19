4ed79cd54c980f66dc228660391d646c33a448d2
Halim Oumaima <oumaimalmao23@gmail.com>
2023-04-19 03:34:52 +0000

    update

--- src/exec/execute.c
+++ src/exec/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ohalim <ohalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 07:38:22 by belkarto          #+#    #+#             */
-/*   Updated: 2023/04/18 05:33:00 by belkarto         ###   ########.fr       */
+/*   Updated: 2023/04/19 03:04:58 by ohalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,6 +110,7 @@ pid_t	exec_one(t_cmd_tab cmd, char **env)
 			open_files(cmd.redir);
 			if (cmd.cmd == NULL)
 				exit(127);
+			ft_printf("cmd.env: %s\n", cmd.env);
 			if (execve(cmd.env, cmd.cmd, env) == -1)
 				put_error("ERROR : exeve", true);
 		}
